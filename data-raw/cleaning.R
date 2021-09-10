19b08997a21661dfa19b5e514b0afc5d1538df55
Eddie Chapman <eddie.chapman4@gmail.com>
2021-09-10 10:35:32 -0500

    Add Atlas coding data

--- data-raw/cleaning.R
+++ data-raw/cleaning.R
@@ -1,28 +1,53 @@
 library(dplyr)
 
-df <- readr::read_csv('data-raw/degree_program_metadata.csv')
+degree_metadata_path <- file.path('data-raw', 'degree_program_metadata.csv')
 
-# Rename columns and drop unwanted columns
-df <- select(df,
-  'degree_id' = 'ID', 
-  'degree_level' = 'DEGREE LEVEL',
-  'degree_name' = 'DEGREE',
-  'degree_type' = 'DEGREE RECODED',
-  'degree_url' = 'URL', 
-  'degree_category' = 'CATEGORY', # what is the difference between `CATEGORY` and 'DEGREE RECODED'?
-  'degree_data_sci_flag' = 'DATA SCIENCE',
-  'department_name' = 'DEPARTMENT',
-  'department_type' = 'DEPARTMENT RECODED',
-  'institution_name' = 'UNIVERSITY', 
-  'institution_carnegie' = 'CARNEGIE',
-  'institution_state' = 'STATE',
-  'institution_city' = 'CITY'
-)
+degrees_df <- 
+  readr::read_csv(degree_metadata_path) %>%
+  select(
+    degree_id = ID, 
+    degree_level = 'DEGREE LEVEL',
+    degree_name = DEGREE,
+    degree_type = 'DEGREE RECODED',
+    degree_url = URL, 
+    degree_category = CATEGORY,    # what is the difference between `CATEGORY` and DEGREE RECODED?
+    degree_data_sci_flag = 'DATA SCIENCE',
+    department_name = DEPARTMENT,
+    department_type = 'DEPARTMENT RECODED',
+    institution_name = UNIVERSITY, 
+    institution_carnegie = CARNEGIE,
+    institution_state = STATE,
+    institution_city = CITY
+  ) %>%
+  mutate(
+    degree_level = recode(degree_level, 'C' = 'certificate', 'B' = 'bachelors', 'M' = 'masters', 'D' = 'doctoral'),
+    degree_id = as.numeric(degree_id)
+  )
 
 
-# Rename the `degree_level` values from abbreviations to full words
-df <- mutate(df, 
-  degree_level = recode(degree_level, 'C' = 'certificate', 'B' = 'bachelors', 'M' = 'masters', 'D' = 'doctoral')
-)
+coding_data_path <- file.path('data-raw', 'atlas_coding_data.csv')
 
-readr::write_csv(df, file='data/degree_programs.csv')
+quotes_df <- 
+  readr::read_csv(coding_data_path) %>%
+  mutate(degree_id = as.numeric(substr(document, 1, 4)),
+         quote_id = group_indices(., quotation)) %>%
+  select(quote_id, degree_id, quote = quotation, codes, comment)
+
+
+codes_df <-
+  quotes_df %>%
+  mutate(codes = stringr::str_split(codes, ', ')) %>%
+  tidyr::unnest_longer(codes) %>%
+  select(quote_id, degree_id, code = codes)
+
+
+quotes_df <- select(quotes_df, quote_id, degree_id, quote, comment)
+
+
+degrees_out_path <- file.path('data', 'degrees.csv')
+quotes_out_path <- file.path('data', 'quotes.csv')
+codes_out_path <- file.path('data', 'codes.csv')
+
+readr::write_csv(degrees_df, file = degrees_out_path)
+readr::write_csv(quotes_df, file = quotes_out_path)
+readr::write_csv(codes_df, file = codes_out_path)
